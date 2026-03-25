8dbb6e6a1d840b0aa08949c82270e5f74e4b5db5
Nisal Dilshan <bcnisal@gmail.com>
2026-03-26 01:34:06 +0900

    cleanup

--- src/RenderSys/Vulkan/Pipeline/VulkanPbrRenderPipeline.cpp
+++ src/RenderSys/Vulkan/Pipeline/VulkanPbrRenderPipeline.cpp
@@ -37,22 +37,12 @@ PbrRenderPipeline::~PbrRenderPipeline()
 
 void PbrRenderPipeline::CreatePipelineLayout(const std::vector<VkDescriptorSetLayout> &descriptorSetLayouts)
 {
-    // VkPushConstantRange pushConstantRange0{};
-    // pushConstantRange0.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
-    // pushConstantRange0.offset = 0;
-    // pushConstantRange0.size = sizeof(VertexCtrl);
-
-    // VkPushConstantRange pushConstantRange1{};
-    // pushConstantRange1.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
-    // pushConstantRange1.offset = sizeof(VertexCtrl);
-    // pushConstantRange1.size = sizeof(PbrMaterial::PbrMaterialProperties);
-
     VkPushConstantRange pushConstantRange{};
     pushConstantRange.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
     pushConstantRange.offset = 0;
     pushConstantRange.size = sizeof(RenderSys::MaterialProperties);
 
-    std::array<VkPushConstantRange, 1> pushConstantRanges = {pushConstantRange}; //{pushConstantRange0, pushConstantRange1};
+    std::array<VkPushConstantRange, 1> pushConstantRanges = {pushConstantRange};
 
     VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
